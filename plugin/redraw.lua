c8602083314e5887761aff44cdecadf7cb9ead76
wongxy <xiyao.wong@foxmail.com>
2024-06-19 14:43:23 +0800

    redraw - more events

--- plugin/redraw.lua
+++ plugin/redraw.lua
@@ -6,23 +6,8 @@ end
 -- Redraw the whole screen to fix potiential highlighting issues --
 -------------------------------------------------------------------
 
--- local ns = vim.api.nvim_create_namespace 'vscode_neovim_custom_redraw_ns'
---
--- local timer
--- local redrawed = false
--- local function on_win()
---   if timer and timer:is_active() then
---     timer:close()
---   end
---
---   timer = vim.defer_fn(function()
---     redrawed = not redrawed
---     if redrawed then
---       vim.cmd.mode()
---     end
---   end, 3000)
--- end
---
--- vim.api.nvim_set_decoration_provider(ns, { on_end = on_win })
-
-vim.cmd [[au CursorHold,InsertLeave * silent mode]]
+vim.api.nvim_create_autocmd({ 'CursorHold', 'TextChanged', 'InsertLeave' }, {
+  callback = function()
+    vim.defer_fn(vim.cmd.mode, 100)
+  end,
+})
