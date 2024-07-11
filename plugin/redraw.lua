483d8ac2c6d6d88d4cb34c8ad14114ad3924434b
wongxy <xiyao.wong@foxmail.com>
2024-07-11 10:44:14 +0800

    redraw - WinScrolled; decrease timeout; debounce

--- plugin/redraw.lua
+++ plugin/redraw.lua
@@ -6,8 +6,19 @@ end
 -- Redraw the whole screen to fix potiential highlighting issues --
 -------------------------------------------------------------------
 
-vim.api.nvim_create_autocmd({ 'CursorHold', 'TextChanged', 'InsertLeave' }, {
-  callback = function()
-    vim.defer_fn(vim.cmd.mode, 100)
-  end,
+local timer
+local function redraw()
+  if timer and timer:is_active() then
+    timer:close()
+  end
+  timer = vim.defer_fn(vim.cmd.mode, 50)
+end
+
+vim.api.nvim_create_autocmd({
+  'CursorHold',
+  'TextChanged',
+  'InsertLeave',
+  'WinScrolled',
+}, {
+  callback = redraw,
 })
